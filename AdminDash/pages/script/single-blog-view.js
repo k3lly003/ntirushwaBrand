6c248811800c4ca88d3760be0ad8c68057a78323
k3lly003 <ntirukelly@gmail.com>
2024-03-07 14:22:58 +0200

    first phase of intergration

--- AdminDash/pages/script/single-blog-view.js
+++ AdminDash/pages/script/single-blog-view.js
@@ -2,6 +2,9 @@ let parentdiv = document.getElementsByClassName("display-input")[0];
 let childdiv = document.getElementsByClassName("child")[0];
 let btn = document.getElementById("display");
 let close = document.getElementsByClassName("no")[0];
+let addLikes = document.getElementById("addLike");
+let displayLikes = document.getElementById("displayLikes");
+let comments = document.getElementById("addComment");
 btn.addEventListener("click", () => {
   parentdiv.style.display = "block";
 });
@@ -11,11 +14,30 @@ close.addEventListener("click", () => {
 /*singleBlog*/
 const searchParams = new URLSearchParams(window.location.search);
 const blogId = searchParams.get("id");
-let data = [];
-let blogList = [];
-data = localStorage.getItem("blogs");
-blogList = JSON.parse(data);
-document.getElementById("title").innerText = blogList[blogId].title;
-document.getElementById("date").innerText = blogList[blogId].date;
-document.getElementById("image").setAttribute("src", blogList[blogId].image);
-document.getElementById("over-text").innerHTML = blogList[blogId].content;
+// let data = [];
+// let = [];
+// data = localStorage.getItem("blogs");
+// blogList = JSON.parse(data);
+
+//fetch
+// const searchParams = new URLSearchParams(window.location.search);
+// const blogId=searchParams.get('id');
+
+fetch(`http://localhost:8000/api/blogs/${blogId}/read`)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    document.getElementById("title").innerText = data.title;
+    document.getElementById("date").innerText = data.createdAt;
+    document.getElementById("image").setAttribute("src", data.image);
+    document.getElementById("over-text").innerHTML = data.content;
+    addLikes.addEventListener("click", () => {
+      !addLikes == false
+        ? (displayLikes.textContent = data.likes + 1)
+        : (displayLikes.textContent = data.likes - 1);
+    });
+    const comment = data.comments;
+    console.log(comment);
+  });
